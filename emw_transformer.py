82c8fa9acafb091f67472abd5c287cb2714c50b8
Vadász Noémi <vadasznoemi@gmail.com>
2018-08-31 10:09:09 +0200

    Add files via upload

--- emw_transformer.py
+++ emw_transformer.py
@@ -11,18 +11,20 @@ import sys
 class Token:
 
     def __init__(self):
-        self.id = ''
-        self.index = ''
-        self.word = ''
+        self.Id = ''
+        self.StartNode = ''
+        self.EndNode = ''
+        self.string = ''
+        self.anas = ''
         self.lemma = ''
         self.pos = ''
-        self.morf = ''
-        self.dep_id = ''
-        self.dep_index = ''
-        self.dep_type = ''
+        self.hfstana = ''
+        self.feature = ''
+        self.depTarget = ''
+        self.depType = ''
 
     def print_ana(self, ofile):
-        print(self.index, self.word, self.lemma, self.pos, self.morf, self.dep_index, self.dep_type, sep='\t', file=ofile)
+        print(self.Id, self.StartNode, self.EndNode, self.string, self.anas, self.lemma, self.pos, self.hfstana, self.feature, self.depTarget, self.depType, sep='\t', end='\n', file=ofile)
 
 
 def xml_parse(ifile):
@@ -33,27 +35,32 @@ def xml_parse(ifile):
     root = tree.getroot()
 
     for annots in root.findall('AnnotationSet'):
-        counter = 0
+
         for annot in annots.findall('Annotation'):
             typ = annot.get('Type')
             if typ == 'Token':
-                counter += 1
+
                 token_ana = Token()
-                token_ana.id = annot.get('Id')
-                token_ana.index = counter
+                token_ana.Id = annot.get('Id')
+                token_ana.StartNode = annot.get('StartNode')
+                token_ana.EndNode = annot.get('EndNode')
                 for feature in annot:
                     if feature[0].text == 'string':
-                        token_ana.word = feature[1].text
+                        token_ana.string = feature[1].text
+                    elif feature[0].text == 'anas':
+                        token_ana.anas = feature[1].text
                     elif feature[0].text == 'lemma':
                         token_ana.lemma = feature[1].text
                     elif feature[0].text == 'pos':
                         token_ana.pos = feature[1].text
                     elif feature[0].text == 'feature':
-                        token_ana.morf = feature[1].text
+                        token_ana.feature = feature[1].text
+                    elif feature[0].text == 'hfstana':
+                        token_ana.hfstana = feature[1].text
                     elif feature[0].text == 'depTarget':
-                        token_ana.dep_id = feature[1].text
+                        token_ana.depTarget = feature[1].text
                     elif feature[0].text == 'depType':
-                        token_ana.dep_type = feature[1].text
+                        token_ana.depType = feature[1].text
                 tsv_annot.append(token_ana)
 
             # üres sor (7 üres mező tabbal)
@@ -61,12 +68,6 @@ def xml_parse(ifile):
                 counter = 0
                 space_ana = Token()
                 tsv_annot.append(space_ana)
-
-    # az id-k helyett folytatólagos indexelést csinál (a deptex miatt kell!)
-    for line in tsv_annot:
-        for line2 in tsv_annot:
-            if line.dep_id == line2.id:
-                line.dep_index = line2.index
 
     return tsv_annot
 
@@ -80,8 +81,8 @@ def print_tsv(tsv_annot, ofile):
 
 def main():
 
-    ifile = sys.argv[1]
-    ofile = sys.argv[2]
+    ifile = 'maistanultam_1_emw_emdep.xml'
+    ofile = 'maistanultam_1_emw_emdep.tsv'
     tsv_annot = xml_parse(ifile)
     print_tsv(tsv_annot, ofile)
 
